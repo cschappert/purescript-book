c8ff97d08fa0d44a66ce4749c7395ba9c4d5d9b4
Chris Schappert <christophe.schappert@rakuten.com>
2019-12-09 14:57:10 +0900

    add some notes and improve chapter3 AddressBook code

--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -7,17 +7,23 @@ import Data.List (List(..), filter, head)
 import Data.Maybe (Maybe)
 
 type Address =
+  -- from { to } is a record. similar to JS Objects, Structs, etc.
+  -- a record can contain another record
   { street :: String
   , city   :: String
   , state  :: String
   }
 
+-- this is a type synonym
+-- a more simple example - type Name = String
 type Entry =
   { firstName :: String
   , lastName  :: String
   , address   :: Address
   }
 
+-- a linked list of Entry records. List is distinct from Array
+-- apply the type constructor List to Entry to get a list of entries
 type AddressBook = List Entry
 
 showAddress :: Address -> String
@@ -26,9 +32,14 @@ showAddress addr = addr.street <> ", " <> addr.city <> ", " <> addr.state
 showEntry :: Entry -> String
 showEntry entry = entry.lastName <> ", " <> entry.firstName <> ": " <> showAddress entry.address
 
+printEntry :: String -> String -> AddressBook -> Maybe String
+printEntry firstName lastName book = map showEntry (findEntry firstName lastName book)
+
 emptyBook :: AddressBook
 emptyBook = empty
 
+-- using eta conversion to write function in point-free form.
+-- other than the restrictions on type, insertEntry is the same function as Cons
 insertEntry :: Entry -> AddressBook -> AddressBook
 insertEntry = Cons
 
